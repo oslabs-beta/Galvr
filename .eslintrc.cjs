4cdd63ebd78923368deae3c6cfaf3fe853770050
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-05-16 21:47:49 -0500

    initial

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  root: true,
+    extends: ['eslint:recommended'],
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    root: true,
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            parserOptions: {
+                project: ['./tsconfig.json'],
+            },
+        },
+    ],
 };
