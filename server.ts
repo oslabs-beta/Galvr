a184a32401705af7a493fcf49c7525ea8933d441
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-05-16 21:33:34 -0500

    init

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const app: Express = express();
 function getRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
+debugger;
 app.get('/rolldice', (req, res) => {
     res.send(getRandomNumber(1, 6).toString());
 });
