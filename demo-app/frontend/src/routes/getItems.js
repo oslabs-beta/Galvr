9dfa3b077ab4792a6f4d891cb3130930bb40334b
Yi Sun <timyisun@gmail.com>
2023-05-20 15:28:48 -0500

    Added the getMidSvc microservice between frontend and backend, on getItem route.

--- demo-app/frontend/src/routes/getItems.js
+++ demo-app/frontend/src/routes/getItems.js
@@ -1,5 +1,7 @@
 module.exports = async (req, res) => {
-    const items = await fetch(`http://${process.env.BB_BACKEND_SERVICE_HOST}:${process.env.BB_BACKEND_SERVICE_PORT}/items`);
-    const data = await items.json()
-    res.send(data);
+  const items = await fetch(
+    `http://${process.env.BB_GETMIDSVC_SERVICE_HOST}:${process.env.BB_GETMIDSVC_SERVICE_PORT}`
+  );
+  const data = await items.json();
+  res.send(data);
 };
