33d473141937c909beb24d7c57a29c0a99f764b7
Djaworski <jaworskidaniel1@gmail.com>
2023-05-25 20:07:02 -0500

    testmidsvc made to familiarize myself with the demo-app

---
+++ demo-app/testmidsvc/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const backend = require('./backendUrl');
+
+const app = express();
+
+app.use(express.json());
+
+// Forward getItem request to backend
+app.get('/items', async (req, res, next) => {
+  try {
+    const temp = fetch()
+    console.log('Test Microservice has been encountered')
+    res.status(200);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.log(`Error: ${err}`);
+  res.status(400);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
