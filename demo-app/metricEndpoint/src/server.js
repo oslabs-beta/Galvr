2c145c1d6c54fa1af4af717b23cf9486880adfbe
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-05-25 22:04:05 -0500

    implemented protobuf in endpoint

--- demo-app/metricEndpoint/src/server.js
+++ demo-app/metricEndpoint/src/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const protobuf = require('protobufjs');
 
 const app = express();
 
-app.use('/*', (req, res) => {
-  console.log(req);
-  res.status(200).send();
-});
+async function run() {
+  const root = await protobuf.load('src/metrics_service.proto');
+  const metricRequest = root.lookupType(
+    'opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest'
+  );
+
+  const metricResponse = root.lookupType(
+    'opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse'
+  );
+
+  app.use('/v1/metrics', (req, res) => {
+    console.log(req.body);
+    if (req.body) {
+      const metrics = metricRequest.decode(Buffer.from(req.body));
+      console.log(metrics);
+    }
+    res
+      .set('Content-Type', 'application/x-protobuf')
+      .send(metricResponse.encode(metricResponse.create({})).finish());
+  });
+}
+
+run().catch((err) => console.log(err));
 
 // Error handler
 // app.use((err, req, res, next) => {
