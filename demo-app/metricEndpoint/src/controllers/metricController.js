732cab702af976c0d91d54f1b66072f61acce398
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-06-03 16:40:41 -0500

    parsing is fully functional

--- demo-app/metricEndpoint/src/controllers/metricController.js
+++ demo-app/metricEndpoint/src/controllers/metricController.js
@@ -6,22 +6,95 @@ metricController.metricDecoder = (req, res, next) => {
   try {
     if (req.body) {
       const metric = metricRequest.decode(req.body);
-      res.locals.metric = metric;
+      res.locals.metrics = metric;
     }
     return next();
   } catch (err) {
-    return next({ log: err, message: 'Error decoding metric' });
+    return next({ log: err, message: 'Error decoding metrics' });
+  }
+};
+
+metricController.metricParser = (req, res, next) => {
+  try {
+    if (res.locals.metrics) {
+      const attributeParser = (arr) =>
+        arr.reduce((obj, cur) => {
+          const curObj = {};
+          const curValue = Object.values(cur.value)[0];
+          curObj[cur.key] = curValue;
+          return Object.assign(obj, curObj);
+        }, {});
+
+      const dataTypes = {
+        gauge: 'gauge',
+        sum: 'sum',
+        histogram: 'histogram',
+        exponential_histogram: 'exponentialHistogram',
+        summary: 'summary',
+      };
+      res.locals.metrics = res.locals.metrics.resourceMetrics.reduce(
+        (resourceMetricsArr, resourceMetricsCur) => {
+          const curResourceMetricObj = {};
+
+          curResourceMetricObj.resource = attributeParser(
+            resourceMetricsCur.resource.attributes
+          );
+
+          curResourceMetricObj.scopeMetrics =
+            resourceMetricsCur.scopeMetrics.reduce(
+              (scopeMetricsObj, scopeMetricsCur) => {
+                const scopeMetricCurObj = {};
+                if (
+                  Object.hasOwn(scopeMetricsCur, 'metrics') &&
+                  scopeMetricsCur.metrics.length > 0
+                ) {
+                  scopeMetricCurObj[scopeMetricsCur.scope.name] =
+                    scopeMetricsCur.metrics.reduce((metricsArr, metricsCur) => {
+                      const metricsCurCopy = structuredClone(metricsCur);
+                      Object.keys(metricsCurCopy).forEach((key) => {
+                        if (Object.hasOwn(dataTypes, key)) {
+                          metricsCurCopy[key].dataPoints = metricsCurCopy[
+                            key
+                          ].dataPoints.reduce((arr, dataPoint) => {
+                            const dataPointCopy = structuredClone(dataPoint);
+                            dataPointCopy.attributes = attributeParser(
+                              dataPointCopy.attributes
+                            );
+                            arr.push(dataPointCopy);
+                            return arr;
+                          }, []);
+                        }
+                      });
+                      metricsArr.push(metricsCurCopy);
+                      return metricsArr;
+                    }, []);
+                }
+                return Object.assign(scopeMetricsObj, scopeMetricCurObj);
+              },
+              {}
+            );
+
+          resourceMetricsArr.push(curResourceMetricObj);
+          return resourceMetricsArr;
+        },
+        []
+      );
+    }
+    return next();
+  } catch (err) {
+    return next({ log: err, status: 502, message: 'Error parsing metrics' });
   }
 };
 
 metricController.metricSaver = (req, res, next) => {
   try {
-    if (res.locals.metric) {
-      console.log('metric: ', JSON.stringify(res.locals.metric));
+    if (res.locals.metrics) {
+      console.log('resource: ', res.locals.metrics[0].resource);
+      console.log('scopeMetrics: ', res.locals.metrics[0].scopeMetrics);
     }
     return next();
   } catch (err) {
-    return next({ log: err, status: 502, message: 'Error saving metric' });
+    return next({ log: err, status: 502, message: 'Error saving metrics' });
   }
 };
 
--- demo-app/metricEndpoint/src/routers/metricRouter.js
+++ demo-app/metricEndpoint/src/routers/metricRouter.js
@@ -3,12 +3,13 @@ const express = require('express');
 const { metricResponse } = require('../proto');
 const {
   metricDecoder,
+  metricParser,
   metricSaver,
 } = require('../controllers/metricController');
 
 const router = express.Router();
 
-router.post('/', metricDecoder, metricSaver, (req, res) => {
+router.post('/', metricDecoder, metricParser, metricSaver, (req, res) => {
   res
     .set('Content-Type', 'application/x-protobuf')
     .send(metricResponse.encode(metricResponse.create({})).finish());
