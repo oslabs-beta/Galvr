14f46e8b5c134842886deab661f9ed6a87dec8d5
Sharon Chen <sharon1272@gmail.com>
2023-06-06 22:33:47 -0400

    finished MVP UI

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Nav from '@/components/Nav';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,61 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <div>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
+
+// export default function RootLayout({children}:
+//   {children: React.ReactNode;}): JSX.Element {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}
+//       <div>
+//         <div>
+//           <main className = "app">
+//             <Nav />
+//           </main>
+//         </div>
+//       </div>
+//       </body>
+//     </html>
+//   );
+// }
+// import '@styles/globals.css';
+
+// import Nav from '@/components/Nav.tsx';
+// import Provider from '@/components/Provider';
+
+// export const metadata = {
+//   title: 'Promptopia',
+//   description: 'Discover & Share AI Prompts',
+// };
+
+// const RootLayout = ({ children }) => (
+//   <html lang="en">
+//     <body>
+//       <Provider>
+//         <div className="main">
+//           <div className="gradient" />
+//         </div>
+
+//         <main className="app">
+//           <Nav />
+//           {children}
+//         </main>
+//       </Provider>
+//     </body>
+//   </html>
+// );
+
+// export default RootLayout;
---
+++ client/components/Graph.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Graph(): JSX.Element {
+  return <div>Graph</div>;
+}
