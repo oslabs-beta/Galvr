b651010243ae2af3793d6c62befbccca88319665
Yi Sun <timyisun@gmail.com>
2023-06-06 22:08:10 -0500

    Merged with Homepage developed by Sharon.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,10 @@
 // import Image from 'next/image';
 // eslint-disable-next-line import/no-unresolved
-import Graph from '@/components/Graph.tsx';
+import Graph from '@/components/Graph';
 
 export default function Home(): JSX.Element {
   return (
-    <section className="w-full flex-center flex-col" class ="bg-sky-500/100">
+    <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Galvr
         <br className="max-md:hidden" />
@@ -12,14 +12,17 @@ export default function Home(): JSX.Element {
           Performance Metrics & Visualization
         </span>
       </h1>
-      <p className="desc text-center">Galvr creates and collect telemetry data from your services and translates that into a valuable visualization dashboard in order to help users swiftly analyze performance and identify latency.  </p>
+      <p className="desc text-center">
+        Galvr creates and collect telemetry data from your services and
+        translates that into a valuable visualization dashboard in order to help
+        users swiftly analyze performance and identify latency.{' '}
+      </p>
 
       <Graph />
     </section>
   );
 }
 
-
-// page: Name, Title, Description 
+// page: Name, Title, Description
 // content: 3-4 links to histogram other graphs
 // header and footer
