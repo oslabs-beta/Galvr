c2a6ade68e22d7b643cc37b4a78aeebedcf824ac
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-06-07 21:47:34 -0500

    dummy server added

--- metricEndpoint/src/controllers/metricController.js
+++ metricEndpoint/src/controllers/metricController.js
@@ -1,4 +1,5 @@
 const { metricRequest } = require('../proto');
+const metricModel = require('../models/metricModel');
 
 const metricController = {};
 
@@ -6,7 +7,11 @@ metricController.metricDecoder = (req, res, next) => {
   try {
     if (req.body) {
       const metric = metricRequest.decode(req.body);
-      res.locals.metrics = metric;
+      res.locals.metrics = metricRequest.toObject(metric, {
+        enums: String, // enums as string names
+        longs: String, // longs as strings (requires long.js)
+        bytes: String, // bytes as base64 encoded strings
+      });
     }
     return next();
   } catch (err) {
@@ -20,18 +25,20 @@ metricController.metricParser = (req, res, next) => {
       const attributeParser = (arr) =>
         arr.reduce((obj, cur) => {
           const curObj = {};
-          const curValue = Object.values(cur.value)[0];
+          let curValue = Object.values(cur.value)[0];
+          if (
+            typeof curValue === 'object' &&
+            Object.hasOwn(curValue, 'values')
+          ) {
+            curValue = curValue.values.reduce((nestArr, nestObj) => {
+              nestArr.push(Object.values(nestObj)[0]);
+              return nestArr;
+            }, []);
+          }
           curObj[cur.key] = curValue;
           return Object.assign(obj, curObj);
         }, {});
 
-      const dataTypes = {
-        gauge: 'gauge',
-        sum: 'sum',
-        histogram: 'histogram',
-        exponential_histogram: 'exponentialHistogram',
-        summary: 'summary',
-      };
       res.locals.metrics = res.locals.metrics.resourceMetrics.reduce(
         (resourceMetricsArr, resourceMetricsCur) => {
           const curResourceMetricObj = {};
@@ -52,7 +59,7 @@ metricController.metricParser = (req, res, next) => {
                     scopeMetricsCur.metrics.reduce((metricsArr, metricsCur) => {
                       const metricsCurCopy = structuredClone(metricsCur);
                       Object.keys(metricsCurCopy).forEach((key) => {
-                        if (Object.hasOwn(dataTypes, key)) {
+                        if (Object.hasOwn(metricsCurCopy[key], 'dataPoints')) {
                           metricsCurCopy[key].dataPoints = metricsCurCopy[
                             key
                           ].dataPoints.reduce((arr, dataPoint) => {
@@ -86,14 +93,42 @@ metricController.metricParser = (req, res, next) => {
   }
 };
 
-metricController.metricSaver = (req, res, next) => {
+metricController.metricSaver = async (req, res, next) => {
   try {
     if (res.locals.metrics) {
-      console.log(JSON.stringify(res.locals.metrics));
+      res.locals.metrics.forEach(async (metric) => {
+        const curMetric = await metricModel.findOne({
+          serviceName: metric.resource['service.name'],
+        });
+        if (curMetric) {
+          await metricModel.findByIdAndUpdate(curMetric._id, {
+            metrics: metric,
+          });
+        } else {
+          await metricModel.create({
+            serviceName: metric.resource['service.name'],
+            metrics: metric,
+          });
+        }
+      });
     }
     return next();
   } catch (err) {
     return next({ log: err, status: 502, message: 'Error saving metrics' });
+  }
+};
+
+metricController.metricGetter = async (req, res, next) => {
+  try {
+    const metrics = await metricModel.find({});
+    res.locals.metrics = metrics.reduce((arr, cur) => {
+      arr.push(cur.metrics);
+      return arr;
+    }, []);
+    console.log(res.locals.metrics);
+    return next();
+  } catch (err) {
+    return next({ log: err, message: 'Error geting metrics' });
   }
 };
 
