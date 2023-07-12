2d91085122417600b8ca37b64b6129f6541d5f84
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-07-12 10:11:55 -0500

    added supertest

--- metricEndpoint/src/server.ts
+++ metricEndpoint/src/server.ts
@@ -71,4 +71,7 @@ app.use(
   }
 );
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+if (process.env.NODE_ENV !== 'test')
+  app.listen(3000, () => console.log('Listening on port 3000'));
+
+export default app;
--- tests/unit.test.ts
+++ tests/unit.test.ts
@@ -1,17 +1,24 @@
+/* eslint-disable import/no-relative-packages */
 import mongoose, { type Mongoose } from 'mongoose';
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-// eslint-disable-next-line import/no-relative-packages
+import { type Server } from 'http';
+
 import { testModel } from '../metricEndpoint/src/models/metricModel';
+import { ExportMetricsServiceRequest } from '../metricEndpoint/src/proto/metricTypes';
+import app from '../metricEndpoint/src/server';
 
 let con: Mongoose;
 let mongoServer: MongoMemoryServer;
+let server: Server;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   con = await mongoose.connect(mongoServer.getUri(), {});
+  server = app.listen(3000);
 });
 
-beforeEach(async () => testModel.collection.drop());
+// beforeEach(async () => testModel.collection.drop());
 
 afterAll(async () => {
   if (con) {
@@ -20,11 +27,20 @@ afterAll(async () => {
   if (mongoServer) {
     await mongoServer.stop();
   }
+  if (server) {
+    server.close();
+  }
 });
 
-describe('Test if jest works likes this', () => {
-  it('should work good', async () => {
-    const data = await testModel.find({});
-    expect(data.length).toEqual(0);
+describe('Interactions with Otel Collector', () => {
+  it('should respond respond to empty request with success', async () => {
+    const response = await request(server)
+      .post('/v1/metrics')
+      .send(
+        ExportMetricsServiceRequest.encode(ExportMetricsServiceRequest.create())
+      )
+      .set('Content-Type', 'application/x-protobuf');
+    expect(response.headers['Content-Type']).toMatch(/application\/x-protobuf/);
+    expect(response.status).toEqual(200);
   });
 });
